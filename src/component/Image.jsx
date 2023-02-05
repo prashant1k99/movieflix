a205af73aec4b1421cfaab70c7e79b2261a2fbb2
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2023-02-05 18:45:26 +0530

    CHORE: added transition property for image hover

--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -7,15 +7,11 @@ const ImageCard = () => {
   );
   useEffect(() => {}, []);
   return (
-    <div className="bg rounded-3xl relative w-48 bg-bottom hover:w-[12.125rem] hover:brightness-110 hover:drop-shadow-2xl">
-      <img
-        className="bg rounded-3xl gradient-to-r hover:from-slate-800"
-        src={source}
-      />
-      <div className="absolute rounded-3xl w-[150%] rotate-[270deg] h-[67%] bottom-[15%] left-[-25%] bg-bottom bg-gradient-to-r from-black"></div>
-      <div className="absolute rounded-3xl bottom-0 -left-0 w-full h-full">
-        <p className="select-none truncate mt-[135%] pr-2 pl-2">
-          Game of thrones of game of thrones of game of thrones
+    <div className="relative m-2 group rounded-3xl w-48 hover:ease-linear duration-300 hover:scale-105 hover:drop-shadow-2xl">
+      <img className=" rounded-3xl" src={source} />
+      <div className="absolute rounded-3xl opacity-0 bottom-0 left-0 group-hover:transition-transform group-hover:opacity-100 hover:delay-300 duration-300 bg-bottom bg-gradient-to-t from-black">
+        <p className="select-none px-3 pb-4 pt-[100%]">
+          Game of thrones ofasdd game of thrones of game of thrones
         </p>
       </div>
     </div>
