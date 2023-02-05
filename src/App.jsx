7ddac385e6d3b055316a595a1dbd0772e7eca6c6
Prashant <prashantco111@gmail.com>
2023-02-05 19:44:36 +0530

    FIX: Added demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ImageCard from "./component/Image";
 import { serverClient } from "./utils";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("game-of-thrones");
+  // const [searchQuery, setSearchQuery] = useState("game-of-thrones");
   const [data, setData] = useState([]);
   const fetchData = async () => {
     const data = await serverClient()
-      .get(`/search/multi?query=${searchQuery}`)
+      .get(`/trending/all/day`)
       .catch((err) => console.log(err));
     setData(data.results);
   };
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
-    <div className="App">
-      <ImageCard title='Game Of Thrones' imageSrc='https://image.tmdb.org/t/p/w500/7WUHnWGx5OO145IRxPDUkQSh4C7.jpg' />
+    <div className="flex">
+      { data.filter(el => el.poster_path).map(el => {
+        return <ImageCard title={el.name || el.title } imageSrc={`https://image.tmdb.org/t/p/w500${el.poster_path}`} />
+      })}
     </div>
   );
 }
--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -1,12 +1,17 @@
-const ImageCard = ({ title, imageSrc, size }) => {
+const ImageCard = ({ title, imageSrc, size, isHoverable = true }) => {
+  // TODO Add Min and max height as per the size of the screen
+  // TODO handle size prop
   return (
-    <div className="relative m-2 group w-48 hover:ease-linear duration-300 hover:scale-105 hover:drop-shadow-m">
+    <div className={`relative m-2 group w-48 ${ isHoverable ? 'hover:ease-linear duration-300 hover:scale-105 hover:drop-shadow-m' : null }`}>
       <img className="rounded-xl" src={imageSrc} />
-      { title ? <div className="absolute w-full rounded-xl opacity-0 border-radius-0 bottom-0 left-0 group-hover:transition-opacity group-hover:opacity-100 duration-300 bg-bottom bg-gradient-to-t from-black">
-        <p className="select-none px-4 pb-3 pt-[92%] text-xl truncate">
-        {title}
-        </p>
-      </div> : null
+      { 
+        title 
+        ? <div className={ `absolute w-full rounded-xl border-radius-0 bottom-0 left-0 bg-bottom bg-gradient-to-t from-black ${isHoverable ? 'opacity-0 group-hover:transition-opacity group-hover:opacity-100 duration-300 ' : null}`}>
+            <p className="select-none px-4 pb-3 pt-[92%] text-xl truncate">
+              {title}
+            </p>
+          </div> 
+        : null
       }
     </div>
   );
