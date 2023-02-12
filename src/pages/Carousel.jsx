64a1356cb5adb0923c03123f9131364eb890f3c1
Prashant <prashantco111@gmail.com>
2023-02-12 12:43:24 +0530

    CHORE: Added key

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -18,17 +18,15 @@ const Carousel = () => {
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
         {data
           .filter((el) => el.poster_path)
-          .map((el) => {
-            return (
-              <div className=" m-auto ">
-                <ImageCard
-                  title={el.name || el.title}
-                  imageSrc={`https://image.tmdb.org/t/p/w500${el.poster_path}`}
-                  key={el.id}
-                />
-              </div>
-            );
-          })}
+          .map((el) => (
+              <ImageCard
+                className=" m-auto "
+                title={el.name || el.title}
+                imageSrc={`https://image.tmdb.org/t/p/w500${el.poster_path}`}
+                key={el.id}
+              />
+            )
+          )}
       </div>
     </div>
   );
